e69ba775e4ed43801a83dccc93ac6ce7294b914d
Pedro Vinicius Chagas Ribeiro <pedro.ribeiro@mcm.local>
2024-10-11 14:49:25 -0300

    criando os DTO de Setor para Request e Response

---
+++ src/main/java/com/pedrochagas/pcrchamados/dtos/setorDTO/SetorRequestDTO.java
@@ -0,0 +1,13 @@
+package com.pedrochagas.pcrchamados.dtos.setorDTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SetorRequestDTO {
+
+    private String nome;
+}
