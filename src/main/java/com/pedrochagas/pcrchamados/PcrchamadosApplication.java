05717767cb4b5b5bfdb08588b5cb4cefe93004fe
Pedro Vinicius Chagas Ribeiro <pedro.ribeiro@mcm.local>
2024-10-11 10:31:41 -0300

    iniciando projeto

---
+++ src/main/java/com/pedrochagas/pcrchamados/PcrchamadosApplication.java
@@ -0,0 +1,13 @@
+package com.pedrochagas.pcrchamados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcrchamadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcrchamadosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pedrochagas/pcrchamados/PcrchamadosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedrochagas.pcrchamados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcrchamadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
